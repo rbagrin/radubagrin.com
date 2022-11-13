e4d194b517d3ea8bad462fcee7fd1c1022442479
Radu Bagrin <bagrin.radu@gmail.com>
2022-11-12 17:49:23 -0800

    Mandatory Header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState('AAA');
 
   useEffect(() => {
     (async () => {
