cb8ef27a79646b940a174cccd261f8ce9e5fb82b
Radu Bagrin <bagrin.radu@gmail.com>
2023-04-20 17:55:39 +0100

    Data to JSON

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ const DEV_PORT = 4000;
 const PROD_PORT = 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: true });
   await app.listen(PROD_PORT);
 }
 
--- backend/src/modules/alphavantage/alphavantage.service.ts
+++ backend/src/modules/alphavantage/alphavantage.service.ts
@@ -1,25 +1,49 @@
-import { HttpService } from "@nestjs/axios";
-import { Injectable, Logger } from "@nestjs/common";
-import { ApiClientService } from "src/infra/http/api-client/api-client.service";
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import { ApiClientService } from 'src/infra/http/api-client/api-client.service';
 
 const API_KEY = 'UC8183VP0K9OM9QX';
-
 @Injectable()
 export class AlphaVantageClientService extends ApiClientService {
   logger = new Logger('Hexnode API Client');
 
   constructor(public httpService: HttpService) {
-
     super(httpService);
     // Both baseUrl and paths should finish in '/' !
     this.setApiClient(`https://www.alphavantage.co`);
-    this.setHeaders({ 'User-Agent': 'request', accept: 'application/json' })
+    this.setHeaders({
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    });
   }
 
-  async getTickerData(ticker: string, functionName: string = 'TIME_SERIES_INTRADAY_EXTENDED'): Promise<any> {
+  async getTickerData(
+    ticker: string,
+    functionName: string = 'TIME_SERIES_INTRADAY_EXTENDED',
+  ): Promise<any> {
     try {
-      const { data } = await this.httpService.get<any>(`/query?function=${functionName}&symbol=${ticker}&interval=5min&apikey=${API_KEY}`).toPromise();
-      return data;
+      const { data } = await this.httpService
+        .get<any>(
+          `/query?function=${functionName}&symbol=${ticker}&interval=5min&apikey=${API_KEY}`,
+        )
+        .toPromise();
+
+      const rows = data
+        .trim()
+        .split('\n')
+        .map((row) => row.trim());
+      const keys = rows[0].split(',');
+      const objects = [];
+      for (let i = 1; i < rows.length; i++) {
+        const values = rows[i].split(',');
+        const obj = {};
+        for (let j = 0; j < keys.length; j++) {
+          obj[keys[j]] = values[j];
+        }
+        objects.push(obj);
+      }
+
+      return objects;
     } catch (error) {
       throw error;
     }
