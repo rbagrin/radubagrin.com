6a96826d8d30f6639e99a656a5030cbe1f8181cb
rbagrin <bagrin.radu@gmail.com>
2023-06-21 23:18:31 +0100

    CAGR calculator

--- src/content/tools/sections/investment-calculator.section.tsx
+++ src/content/tools/sections/investment-calculator.section.tsx
@@ -2,6 +2,8 @@ import React, { useMemo, useState } from "react";
 import { keepOnlyXDigits } from "../../../util/number.util";
 import { PieChart } from "../../../components/PieChart";
 import { Input } from "../../../components/forms/Input";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export const InvestmentCalculatorSection = () => {
   const [startingSum, setStartingSum] = useState<number | "">(1000);
@@ -12,6 +14,10 @@ export const InvestmentCalculatorSection = () => {
   const [futureValue, setFutureValue] = useState<number | "">(10000);
   const [rateOfReturn, setRateOfReturn] = useState<number | "">(5);
   const [numberOfPeriods, setNumberOfPeriods] = useState<number | "">(10);
+
+  const [endingValue, setEndingValue] = useState<number | "">(100);
+  const [beginningValue, setBeginningValue] = useState<number | "">(100);
+  const [years, setYears] = useState<number | "">(3);
 
   const finalInvestmentValue = useMemo(() => {
     const annualRate = Number(growthRate) / 100;
@@ -46,6 +52,14 @@ export const InvestmentCalculatorSection = () => {
     [futureValue, rateOfReturn, numberOfPeriods]
   );
 
+  const cagr = useMemo(() => {
+    if (!endingValue || !beginningValue || !years) return 0;
+
+    const result = ((endingValue / beginningValue) ** (1 / years) - 1) * 100;
+
+    return keepOnlyXDigits(result, 2);
+  }, [endingValue, beginningValue, years]);
+
   const chartData = useMemo(
     () => ({
       labels: [
@@ -72,7 +86,9 @@ export const InvestmentCalculatorSection = () => {
 
   return (
     <div>
-      <h1>Stocks</h1>
+      <Typography variant="h2" component="h1" sx={{ fontWeight: "bold" }}>
+        Stocks
+      </Typography>
 
       <div
         style={{
@@ -82,8 +98,8 @@ export const InvestmentCalculatorSection = () => {
         }}
       >
         <div>
-          <h2>Investment calculator</h2>
-          <div style={{ display: "flex", gap: "20px" }}>
+          <Typography variant="h3">Investment calculator</Typography>
+          <Box sx={{ display: "flex", gap: "20px", mt: 2 }}>
             <Input
               name="startingSum"
               label="Starting amount:"
@@ -98,8 +114,8 @@ export const InvestmentCalculatorSection = () => {
               value={monthlyInvestment}
               setValue={setMonthlyInvestment}
             />
-          </div>
-          <div style={{ display: "flex", gap: "20px" }}>
+          </Box>
+          <Box sx={{ display: "flex", gap: "20px", mt: 2 }}>
             <Input
               name="investmentYears"
               label="Investment years:"
@@ -114,48 +130,56 @@ export const InvestmentCalculatorSection = () => {
               value={growthRate}
               setValue={setGrowthRate}
             />
-          </div>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              mt: 2,
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h5">
+              Your investment after {Number(investmentYears)} year
+              {Number(investmentYears) === 1 ? "" : "s"}:
+            </Typography>
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              ${finalInvestmentValue}
+            </Typography>
+          </Box>
         </div>
 
-        <div style={{ width: "50%" }}>
-          <div style={{ paddingRight: "80px" }}>
+        <Box sx={{ width: "45%" }}>
+          <Box sx={{ pr: 8 }}>
             <PieChart
               title={
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                    marginBottom: "0",
-                  }}
-                >
-                  <p>
-                    Your investment after {Number(investmentYears)} year
-                    {Number(investmentYears) === 1 ? "" : "s"}:
-                  </p>
-                  <p
-                    style={{
-                      marginLeft: "10px",
-                      fontWeight: "bold",
-                      fontSize: "24px",
-                    }}
-                  >
-                    ${finalInvestmentValue}
-                  </p>
-                </div>
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <Typography variant="h5">End Balance Breakdown</Typography>
+                </Box>
               }
-              subtitle="End Balance Breakdown"
               chartData={chartData}
             />
-          </div>
-        </div>
+          </Box>
+        </Box>
       </div>
 
-      <div>
-        <h2>Present value = {presentValue}</h2>
+      <Box sx={{ mt: 8 }}>
+        <Typography variant="h3">Present value (PV)</Typography>
+        <Typography variant="body1" sx={{ mt: 2 }}>
+          <a
+            href="https://www.investopedia.com/terms/p/presentvalue.asp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Present value
+          </a>{" "}
+          is the current value of a future sum of money or stream of cash flows
+          given a specified rate of return.
+        </Typography>
 
-        <div style={{ display: "flex", gap: "20px" }}>
+        <Box sx={{ display: "flex", gap: 2, alignItems: "end", mt: 2 }}>
           <Input
             label="Future value:"
             name="futureValue"
@@ -174,8 +198,72 @@ export const InvestmentCalculatorSection = () => {
             value={numberOfPeriods}
             setValue={setNumberOfPeriods}
           />
-        </div>
-      </div>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h5">Present value:</Typography>
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              ${presentValue}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box sx={{ mt: 8 }}>
+        <Typography variant="h3">Compound annual growth rate (CAGR)</Typography>
+        <Typography variant="body1" sx={{ mt: 2 }}>
+          The{" "}
+          <a
+            href="https://www.investopedia.com/terms/c/cagr.asp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            compound annual growth rate
+          </a>{" "}
+          is the rate of return (RoR) that would be required for an investment
+          to grow from its beginning balance to its ending balance, assuming the
+          profits were reinvested at the end of each period of the investment’s
+          life span.
+        </Typography>
+        <Box sx={{ display: "flex", gap: 2, alignItems: "end", mt: 2 }}>
+          <Input
+            label="Ending value"
+            name="endingValue"
+            value={endingValue}
+            setValue={setEndingValue}
+          />
+          <Input
+            label="Beginning value"
+            name="beginningValue"
+            value={beginningValue}
+            setValue={setBeginningValue}
+          />
+          <Input
+            label="No of periods"
+            name="years"
+            value={years}
+            setValue={setYears}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h5">CAGR:</Typography>
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              {cagr}%
+            </Typography>
+          </Box>
+        </Box>
+
+        {/*  TODO: Add beginning value date picker & ending value date picker, count years between, get price at that moment, then calculate return */}
+      </Box>
     </div>
   );
 };
