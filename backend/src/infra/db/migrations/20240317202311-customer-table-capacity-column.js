c20b023eee100c4d18e670bf4967ab8795a433bf
Radu Bagrin <bagrin.radu@gmail.com>
2024-03-17 20:29:41 +0000

    Migrations

--- backend/src/infra/db/migrations/20240317202311-customer-table-capacity-column.js
+++ backend/src/infra/db/migrations/20240317202311-customer-table-capacity-column.js
@@ -3,14 +3,14 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up(queryInterface) {
     await queryInterface.addColumn('customer', 'capacity', {
       type: DataTypes.INTEGER,
       allowNull: false,
     });
   },
 
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.removeColumn('customer', 'capacity');
   },
 };
